19322910b5addff37fd985ebc7faef7adcb7576f
Drew Jordan <ajordan12@luc.edu>
2024-04-14 10:55:54 -0500

    updating code based on Ramiz's diag

--- Working/Decompiled/Professor.java
+++ Working/Decompiled/Professor.java
@@ -1,25 +1,71 @@
+// package Working.Decompiled;
+
+// // Source code is decompiled from a .class file using FernFlower decompiler.
+// public class Professor extends Person {
+//     private String department;
+ 
+//     public Professor(String firstName, String lastName, String department) {
+//        super(firstName, lastName);
+//        this.department = department;
+//     }
+ 
+//     public String getDepartment() {
+//        return this.department;
+//     }
+ 
+//     public void setDepartment(String department) {
+//        this.department = department;
+//     }
+ 
+//     public String toString() {
+//        String var10000 = this.getFullName();
+//        return "Professor Name: " + var10000 + " " + this.getDepartment();
+//     }
+//  }
+ 
+
 package Working.Decompiled;
 
-// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Professor extends Person {
     private String department;
- 
+    private List<Course> coursesTaught; // A list to keep track of courses the professor teaches
+
     public Professor(String firstName, String lastName, String department) {
-       super(firstName, lastName);
-       this.department = department;
+        super(firstName, lastName);
+        this.department = department;
+        this.coursesTaught = new ArrayList<>(); // Initialize the list of courses
     }
- 
+
     public String getDepartment() {
-       return this.department;
+        return this.department;
     }
- 
+
     public void setDepartment(String department) {
-       this.department = department;
+        this.department = department;
     }
- 
+
+    // Method to add a course to the list of courses taught by the professor
+    public void addCourse(Course course) {
+        if (!this.coursesTaught.contains(course)) {
+            this.coursesTaught.add(course);
+        }
+    }
+
+    // Method to remove a course from the list of courses taught by the professor
+    public void removeCourse(Course course) {
+        this.coursesTaught.remove(course);
+    }
+
+    // Method to get the list of courses taught by the professor
+    public List<Course> getCoursesTaught() {
+        return this.coursesTaught;
+    }
+
+    @Override
     public String toString() {
-       String var10000 = this.getFullName();
-       return "Professor Name: " + var10000 + " " + this.getDepartment();
+        return "Professor Name: " + getFullName() + " | Department: " + getDepartment();
     }
- }
- 
+}
--- Working/Decompiled/Student.java
+++ Working/Decompiled/Student.java
@@ -1,78 +1,79 @@
-package Working.Decompiled;
+// package Working.Decompiled;
 
-// Source code is decompiled from a .class file using FernFlower decompiler.
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
+// // Source code is decompiled from a .class file using FernFlower decompiler.
+// import java.io.PrintStream;
+// import java.util.ArrayList;
+// import java.util.List;
 
-public class Student extends Person implements Shopper {
-   private int studentId;
-   private List<Course> registeredCourses = new ArrayList();
-   private List<Course> waitListedCourses = new ArrayList();
-   private GroceryList groceryList;
+// public class Student extends Person implements Shopper {
+//    private int studentId;
+//    private List<Course> registeredCourses = new ArrayList();
+//    private List<Course> waitListedCourses = new ArrayList();
+//    private GroceryList groceryList;
 
-   public Student(String firstName, String lastName, int id) {
-      super(firstName, lastName);
-      this.studentId = id;
-      this.groceryList = new GroceryList();
-   }
+//    public Student(String firstName, String lastName, int id) {
+//       super(firstName, lastName);
+//       this.studentId = id;
+//       this.groceryList = new GroceryList();
+//    }
 
-   public int getStudentId() {
-      return this.studentId;
-   }
+//    public int getStudentId() {
+//       return this.studentId;
+//    }
 
-   public void addRegisteredCourse(Course course) {
-      if (!this.registeredCourses.contains(course)) {
-         this.registeredCourses.add(course);
-      }
+//    public void addRegisteredCourse(Course course) {
+//       if (!this.registeredCourses.contains(course)) {
+//          this.registeredCourses.add(course);
+//       }
 
-   }
+//    }
 
-   public void removeCourse(Course course) {
-      this.registeredCourses.remove(course);
-   }
+//    public void removeCourse(Course course) {
+//       this.registeredCourses.remove(course);
+//    }
 
-   public void addWaitListedCourse(Course course) {
-      if (!this.waitListedCourses.contains(course)) {
-         this.waitListedCourses.add(course);
-      }
+//    public void addWaitListedCourse(Course course) {
+//       if (!this.waitListedCourses.contains(course)) {
+//          this.waitListedCourses.add(course);
+//       }
 
-   }
+//    }
 
-   public void removeFromWaitList(Course course) {
-      this.waitListedCourses.remove(course);
-   }
+//    public void removeFromWaitList(Course course) {
+//       this.waitListedCourses.remove(course);
+//    }
 
-   public List<Course> getRegisteredCourses() {
-      return new ArrayList(this.registeredCourses);
-   }
+//    public List<Course> getRegisteredCourses() {
+//       return new ArrayList(this.registeredCourses);
+//    }
 
-   public List<Course> getWaitListedCourses() {
-      return new ArrayList(this.waitListedCourses);
-   }
+//    public List<Course> getWaitListedCourses() {
+//       return new ArrayList(this.waitListedCourses);
+//    }
 
-   public void createGroceryList() {
-      this.groceryList = new GroceryList();
-   }
+//    public void createGroceryList() {
+//       this.groceryList = new GroceryList();
+//    }
 
-   public void addItemToGroceryList(String itemName, int quantity, GroceryStore store) {
-      GroceryItemOrder itemOrder = store.getOrder(itemName, quantity);
-      if (itemOrder != null) {
-         this.groceryList.add(itemOrder);
-      } else {
-         System.out.println("Item " + itemName + " could not be added to the list (not found in store).");
-      }
+//    public void addItemToGroceryList(String itemName, int quantity, GroceryStore store) {
+//       GroceryItemOrder itemOrder = store.getOrder(itemName, quantity);
+//       if (itemOrder != null) {
+//          this.groceryList.add(itemOrder);
+//       } else {
+//          System.out.println("Item " + itemName + " could not be added to the list (not found in store).");
+//       }
 
-   }
+//    }
 
-   public void viewGroceryList() {
-      PrintStream var10000 = System.out;
-      String var10001 = this.getFullName();
-      var10000.println("Grocery List for " + var10001 + ":\n" + this.groceryList);
-   }
+//    public void viewGroceryList() {
+//       PrintStream var10000 = System.out;
+//       String var10001 = this.getFullName();
+//       var10000.println("Grocery List for " + var10001 + ":\n" + this.groceryList);
+//    }
 
-   public String toString() {
-      String var10000 = this.getFullName();
-      return "Student Name: " + var10000 + " | Student ID: " + this.getStudentId();
-   }
-}
+//    public String toString() {
+//       String var10000 = this.getFullName();
+//       return "Student Name: " + var10000 + " | Student ID: " + this.getStudentId();
+//    }
+// }
+
